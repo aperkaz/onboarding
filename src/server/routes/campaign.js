883cc70de376566d2ad583d78b359517912660d5
dzhitomirsky-sc <dzhitomirsky@scand.com>
2017-01-04 12:26:50 +0300

    Added 'like' searching query, added 'inRange' search.

--- src/server/routes/campaign.js
+++ src/server/routes/campaign.js
@@ -1,4 +1,7 @@
 // rest routes for campaigns
+const _ = require('lodash');
+const campaignSearchFields = ['campaignId', 'status', 'campaignType', 'owner'];
+
 module.exports = function(epilogue, db) {
   epilogue.resource({
     model: db.Campaign,
@@ -6,5 +9,37 @@ module.exports = function(epilogue, db) {
       '/campaigns',
       '/campaigns/:campaignId'
     ]
+  }).use({
+    list: {
+      fetch: {
+        before: function(req, res, context) {
+          let searchQuery = {};
+          _.each(campaignSearchFields, (searchField) => {
+            if(!_.isEmpty(req.query[searchField])) {
+              searchQuery[searchField] = {
+                $like: `%${req.query[searchField]}%`
+              }
+            }
+          });
+          if(!_.isUndefined(req.query.startsOn) && !_.isNaN(Date.parse(req.query.startsOn))) {
+            searchQuery.startsOn = {
+              $gte: new Date(req.query.startsOn)
+            }
+          }
+          if(!_.isUndefined(req.query.endsOn) && !_.isNaN(Date.parse(req.query.endsOn))) {
+            searchQuery.endsOn = {
+              $lte: new Date(req.query.endsOn)
+            }
+          }
+          db.Campaign.findAll({
+            where: searchQuery
+          }).then((campaigns) => {
+            // eslint-disable-next-line no-param-reassign
+            context.instance = campaigns;
+            context.skip();
+          })
+        }
+      }
+    }
   });
 };
