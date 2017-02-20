e02a55d57ec7da34d910b5dfd6a3d7d9730158dc
Patryk Kopycinski <contact@patrykkopycinski.com>
2017-02-20 17:10:57 +0100

    Add dot-location rule to .eslintrc
    Update rimraf dependency version

--- src/server/db/migrations/20170219141238-update-campaign-contact-email-migration.js
+++ src/server/db/migrations/20170219141238-update-campaign-contact-email-migration.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
+  up: function(queryInterface, Sequelize) {
     return queryInterface.changeColumn('CampaignContact', 'email', {
       type: Sequelize.STRING,
       allowNull: false,
@@ -12,7 +12,7 @@ module.exports = {
     });
   },
 
-  down: function (queryInterface, Sequelize) {
+  down: function(queryInterface, Sequelize) {
     return queryInterface.changeColumn('CampaignContact', 'email', {
       type: Sequelize.STRING(30),
       allowNull: false,
