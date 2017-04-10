5f8d3df4590f3c47308e87002c684655d6b51f59
gr4per <arne.graeper@opuscapita.com>
2017-04-10 15:38:07 +0200

    #77 fixing image pathes in email

--- src/utils/emailIntegration.js
+++ src/utils/emailIntegration.js
@@ -36,10 +36,10 @@ let sendInvitation = (from, recipient, subject, updateTransitionState, callback)
 					</head>
 					<body style="border: 1px solid #ddd; font-family: avenir;">
 						<a target="_blank" href="${URL}/public/transition/${recipient.campaignId}/${recipient.id}?transition=loaded">
-							<img src="${URL}/images/header.jpg" alt="header image" width="100%">
+							<img src="${URL}/static/images/header.jpg" alt="header image" width="100%">
 						</a>
                         <a target="_blank" href="${URL}/public/transition/${recipient.campaignId}/${recipient.id}?transition=loaded">
-							<img src="${URL}/images/logo.jpg" alt="image with logo" width="100%">
+							<img src="${URL}/static/images/logo.jpg" alt="image with logo" width="100%">
 						</a>
 						<section class="container-fluid">
 							<article style="background: #f9f9f9; padding: 15px; margin:15px;">
@@ -93,8 +93,9 @@ let sendInvitation = (from, recipient, subject, updateTransitionState, callback)
 									<b><a href="#">matts.ek@ncc.se</a></b>
 								</div>
 							</div>
-							<div style="visibility: hidden; display: block; font-size: 0;content: ' '; clear: both; height: 0;"></div>
-							<img style="display:none" src="${emailOpenTrack}" />
+							<div style="visibility: hidden; display: block; font-size: 0;content: ' '; clear: both; height: 0;">
+							        <img style="display:none" src="${emailOpenTrack}" />
+                                                        </div>
 						</section>
 					</body>
 				</html>`
