0cb27d61308ff23ec289b5f870c929808010cf5e
Mateusz <mateusz@whitebird.pl>
2017-05-30 13:22:06 +0200

    companyId allowNull false

--- src/server/db/models/Campaign.js
+++ src/server/db/models/Campaign.js
@@ -39,7 +39,7 @@ module.exports = function(sequelize) {
     },
     companyId: {
       type: Sequelize.BIGINT(20),
-      allowNull: true
+      allowNull: false
     }
   }, {
     getterMethods: {
