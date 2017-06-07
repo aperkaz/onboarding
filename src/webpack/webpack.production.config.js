39e98d130b7035b0f26c486e24e44da1859d3ffe
Patryk Kopycinski <contact@patrykkopycinski.com>
2017-06-07 13:58:45 +0200

    Remove not working loadsh webpack plugin

--- src/webpack/webpack.production.config.js
+++ src/webpack/webpack.production.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 const Config = require('webpack-config').default;
 const AssetsPlugin = require('assets-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 // var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = new Config().extend('./src/webpack/webpack.base.config.js').merge({
@@ -24,20 +23,9 @@ module.exports = new Config().extend('./src/webpack/webpack.base.config.js').mer
       'process.env.NODE_ENV': '"production"'
     }),
     new webpack.optimize.OccurrenceOrderPlugin(true),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        drop_console: true,
-        unsafe: true,
-        pure_getters: true,
-        dead_code: true,
-        unsafe_comps: true,
-        screw_ie8: true
-      }
-    }),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
     new AssetsPlugin({ filename: 'assets.json', path: path.resolve(__dirname, '../../build/client') }),
     new WebpackMd5Hash(),
-    new LodashModuleReplacementPlugin()
     // new BundleAnalyzerPlugin()
   ],
 });
