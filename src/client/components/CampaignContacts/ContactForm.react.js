230717c431a60a0d66daac1cf4bc85042bda4e77
Patryk Kopycinski <contact@patrykkopycinski.com>
2017-06-27 14:06:18 +0200

    #128 Add Country dropdown to Campaign’s Contact form

--- src/client/components/CampaignContacts/ContactForm.react.js
+++ src/client/components/CampaignContacts/ContactForm.react.js
@@ -1,87 +1,103 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Field } from 'redux-form';
 import _ from 'lodash';
-import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
+import { injectIntl, intlShape } from 'react-intl';
+import serviceComponent from '@opuscapita/react-loaders/lib/serviceComponent';
 import { CAMPAIGN_CONTACT_FIELDS } from '../../constants/campaignContacts';
 import FormFieldError from '../common/FormFieldError';
 
-const renderTextInput = (field) => {
-  const { meta: { touched, error } } = field;
-  let hasError = !_.isEmpty(error) && touched;
-  return (
-    <div className={`form-group ${hasError ? 'has-error' : ''}`}>
-      <label className="col-sm-4 control-label" htmlFor={field.name}>{field.label}</label>
-      <div className="col-sm-8">
-        <input
-          readOnly={field.readOnly}
-          disabled={field.disabled}
-          {...field.input}
-          className="form-control"
-          name={field.name}
-          type="text"
+class ContactForm extends Component {
+  componentWillMount() {
+    console.log(this.props);
+
+    const serviceRegistry = (service) => ({ url: `${this.props.actionUrl}/isodata` });
+    const CountryField = serviceComponent({ serviceRegistry, serviceName: 'isodata' , moduleName: 'isodata-countries', jsFileName: 'countries-bundle' });
+
+    this.externalComponents = { CountryField };
+  }
+
+  renderFieldComponent = (field) => {
+    const { CountryField } = this.externalComponents;
+    const { meta: { touched, error } } = field;
+    const hasError = !_.isEmpty(error) && touched;
+    const TextComponent = (
+      <input
+        readOnly={field.readOnly}
+        disabled={field.disabled}
+        {...field.input}
+        className="form-control"
+        name={field.name}
+        type="text"
+      />
+    );
+    const CountryComponent = (
+      <CountryField
+        actionUrl={this.props.actionUrl}
+        {...field.input}
+      />
+    );
+    const component = field.input.name === "country" ? CountryComponent : TextComponent;
+
+    console.log('field', field);
+
+    return (
+      <div className={`form-group ${hasError ? 'has-error' : ''}`}>
+        <label className="col-sm-4 control-label" htmlFor={field.name}>{field.label}</label>
+        <div className="col-sm-8">
+          {component}
+        </div>
+        <FormFieldError
+          hasError={hasError}
+          error={error}
         />
       </div>
-      <FormFieldError
-        hasError={hasError}
-        error={error}
-      />
-    </div>
-  );
-};
+    );
+  }
 
+  render() {
+    const { mode, formLabel, submitButtonLabel, closeButtonLabel, onCancel, onSave, handleSubmit, intl } = this.props;
 
-const ContactForm = ({
-  mode,
-  formLabel,
-  submitButtonLabel,
-  closeButtonLabel,
-  onCancel,
-  onSave,
-  handleSubmit,
-  intl
-}) => {
-  return (
-    <div className="form-horizontal">
-      <h1>
-        {formLabel}
-      </h1>
-      <div className="row">
-        <div className="col-md-12">
-          <Field
-            disabled={mode === 'update'}
-            label={intl.formatMessage({ id: 'campaignContactEditor.contactForm.email.label' })}
-            name="email"
-            component={renderTextInput}
-          />
+    return (
+      <div className="form-horizontal">
+        <h1>
+          {formLabel}
+        </h1>
+        <div className="row">
+          <div className="col-md-12">
+            <Field
+              disabled={mode === 'update'}
+              label={intl.formatMessage({ id: 'campaignContactEditor.contactForm.email.label' })}
+              name="email"
+              component={this.renderFieldComponent}
+            />
 
-          {
-            _.map(
-              _.without(CAMPAIGN_CONTACT_FIELDS, 'email', 'campaignId'),
-              (contactFieldName) => {
-                return (
+            {
+              _.map(
+                _.without(CAMPAIGN_CONTACT_FIELDS, 'email', 'campaignId'),
+                (contactFieldName) => (
                   <Field
                     key={contactFieldName}
                     label={intl.formatMessage({ id: `campaignContactEditor.contactForm.${contactFieldName}.label` })}
                     name={contactFieldName}
-                    component={renderTextInput}
-                    disabled={contactFieldName === "status" ? true : false}
+                    component={this.renderFieldComponent}
+                    disabled={contactFieldName === "status"}
                   />
-                );
-              })
-          }
+                ))
+            }
+          </div>
+        </div>
+        <div className="form-submit text-right">
+          <button className="btn btn-link" onClick={onCancel}>
+            {closeButtonLabel}
+          </button>
+          <button className="btn btn-primary" onClick={handleSubmit(onSave)}>
+            {submitButtonLabel}
+          </button>
         </div>
       </div>
-      <div className="form-submit text-right">
-        <button className="btn btn-link" onClick={onCancel}>
-          {closeButtonLabel}
-        </button>
-        <button className="btn btn-primary" onClick={handleSubmit(onSave)}>
-          {submitButtonLabel}
-        </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 ContactForm.propTypes = {
   mode: PropTypes.oneOf(['create', 'update']).isRequired,
@@ -92,6 +108,10 @@ ContactForm.propTypes = {
   closeButtonLabel: PropTypes.string.isRequired,
   intl: intlShape.isRequired,
   handleSubmit: PropTypes.func.isRequired // this function is injected by redux-form, don't define it by yourself
+};
+
+ContactForm.defaultProps = {
+  actionUrl: window.location.origin
 };
 
 export default injectIntl(ContactForm);
