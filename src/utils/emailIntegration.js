67a36843cb9efe26ad6510b1d5f643e1b98ce096
gr4per <arne.graeper@opuscapita.com>
2017-06-29 09:13:13 +0200

    removed customer hardcoded names and contact

--- src/utils/emailIntegration.js
+++ src/utils/emailIntegration.js
@@ -43,34 +43,34 @@ const sendInvitation = (customer, recipient, updateTransitionState, callback) =>
                   <article style="background: #f9f9f9; padding: 15px; margin:15px;">
                     <h2>Dear ${recipient.contactFirstName} ${recipient.contactLastName} at ${recipient.companyName}</h2>
                     <p style="text-align: justify;">
-                      <i>As part of strenghtening cooperation with you as one of our most important contract partners and to reach our goal of increased internal efficiency such as the development of our purchasing and supplier management NCC Svenska AB will manage all supplier invoices electronically, which will lead to a more efficient invoicing both for the benefit of NCC Svenska AB as for you as our partner.</i>
+                      <i>As part of strenghtening cooperation with you as one of our most important contract partners and to reach our goal of increased internal efficiency such as the development of our purchasing and supplier management ${customer.customerName} will manage all supplier invoices electronically, which will lead to a more efficient invoicing both for the benefit of ${customer.customerName} as for you as our partner.</i>
                     </p>
                     <br>
                     <p style="text-align: justify;"><b>
-                      This is a priority project of NCC Svenska AB (hereinafter named NCC) and an important part in the development of the entire NCC Group where we hope you will develop together with us and see it as an equally important project.
+                      This is a priority project of ${customer.customerName} where we hope you will develop together with us and see it as an equally important project.
                     </b></p>
                     <br>
                     <p style="text-align: justify;">
-                      NCC in cooperation with <a target="_blank" href="http://www.opuscapita.com/">OpusCapita</a> will handle NCC electronic messeging such as supplier invoices and customer invoices. The first step for NCC is the introduction of electronic supplier invoices. When this is implemented fully, other electronic feeds will be revised as customer invoice. NCC will carry out connection work with the help of an intuitive and streamlined process. The process steps and associated description, validation and support can be found after registration on our <a target="_blank" href="${URL}/public/landingpage/c_${recipient.tenantId}/${recipient.campaignName}/${recipient.id}?transition=loaded">Supplier Onboarding</a> site.
+                      ${customer.customerName} in cooperation with <a target="_blank" href="http://www.opuscapita.com/">OpusCapita</a> will handle ${customer.customerName} electronic messeging such as supplier invoices and customer invoices. The first step for ${customer.customerName} is the introduction of electronic supplier invoices. When this is implemented fully, other electronic feeds will be revised as customer invoice. ${customer.customerName} will carry out connection work with the help of an intuitive and streamlined process. The process steps and associated description, validation and support can be found after registration on our <a target="_blank" href="${URL}/public/landingpage/c_${recipient.tenantId}/${recipient.campaignName}/${recipient.id}?transition=loaded">Supplier Onboarding</a> site.
                     </p>
     
                     <hr>
     
                     <h2>1. Registration</h2>
                     <p style="text-align: justify;">
-                      NCC uses OpusCapita's cloud-based supplier onboarding service to establish an electronic invoice flow with you as a preferred supplier. The accession process is initialized by NCC and requires no effort from you as a supplier other than engaging in a dialogue and registration process with OpusCapita on behalf of NCC.
+                      NCC uses OpusCapita's cloud-based supplier onboarding service to establish an electronic invoice flow with you as a preferred supplier. The access process is initialized by ${customer.customerName} and requires no effort from you as a supplier other than engaging in a dialogue and registration process with OpusCapita on behalf of ${customer.customerName}.
                     </p>
     
                     <br>
     
                     <h2>2. Choose your channel of invoice sending</h2>
                     <p style="text-align: justify;">
-                      The easiest way to submit an invoice is as an eInvoice! Always 100% correct data transferred around the clock, all working days - Globally! Connect with us using your existing service provider for e-invoicing by simply choose your operator from our list operators and we will set up your connection in no time. In case of your operator is new to us, OpusCapita takes care of the technical setup with the new operator. We have a global reach as you will be connected to the global and open OpusCapita Business Network. Other channels of submitting of submitting invoice to NCC are "Invoices as PDF via e-mail", "Supplier Portal" - all available on our <a target="_blank" href="${URL}/public/landingpage/c_${recipient.tenantId}/${recipient.campaignName}/${recipient.id}?transition=loaded">Supplier Onboarding site</a> - Where you also find more information associated to service and support.
+                      The easiest way to submit an invoice is as an eInvoice! Always 100% correct data transferred around the clock, all working days - Globally! Connect with us using your existing service provider for e-invoicing by simply choose your operator from our list operators and we will set up your connection in no time. In case of your operator is new to us, OpusCapita takes care of the technical setup with the new operator. We have a global reach as you will be connected to the global and open OpusCapita Business Network. Other channels of submitting of submitting invoice to ${customer.customerName} are "Invoices as PDF via e-mail", "Supplier Portal" - all available on our <a target="_blank" href="${URL}/public/landingpage/c_${recipient.tenantId}/${recipient.campaignName}/${recipient.id}?transition=loaded">Supplier Onboarding site</a> - Where you also find more information associated to service and support.
                     </p>
     
                     By participating in our <a target="_blank" href="${URL}/public/landingpage/c_${recipient.tenantId}/${recipient.campaignName}/${recipient.id}?transition=loaded">Supplier Onboarding program</a> you as a Supplier will benefit of the following:
                     <ul>
-                      <li>Fast and guaranteed delivery of invoices to NCC within a day and you get paid faster since there is no delay to the paper version.</li>
+                      <li>Fast and guaranteed delivery of invoices to ${customer.customerName} within a day and you get paid faster since there is no delay to the paper version.</li>
                       <li>Fewer mistakes - OpusCapita validates your invoices to detect miscalculations and other common errors.</li>
                       <li>Reduce administrative cost - Send your invoice data to NCC instantly and with 100% accuracy.</li>
                       <li>Reducee material cost(e.g., labor, paper, envelopes, printer ink, and postage.)</li>
@@ -85,10 +85,10 @@ const sendInvitation = (customer, recipient, updateTransitionState, callback) =>
                 <section>
                   <div style="margin-top: -10px; float: right; padding-right: 50px; display: inline-block;">
                     <div>
-                      <p style="text-align: justify;">If you have questions regarding this Mail please contact:<br>
-                      <b>Matts Ek.</b> Manager at NCC Svenska AB Holding<br>
-                      <b>+46 761 26 99 79</b><br>
-                      <b><a href="#">matts.ek@ncc.se</a></b>
+                      <!--<p style="text-align: justify;">If you have questions regarding this Mail please contact:<br>
+                      <b>onboarding contact name</b> Onboardig Manager at ${customer.customerName}<br>
+                      <b>onboarding contact phone</b><br>
+                      <b><a href="#">onboarding contact email</a></b>-->
                     </div>
                   </div>
                   <div style="visibility: hidden; display: block; font-size: 0;content: ' '; clear: both; height: 0;">
