48e0a05f4b23a00efb8622f452636b7f2451e614
Dariusz Jakubowski <celber@me.com>
2017-07-11 08:15:06 +0200

    #146 increased default timeout

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -105,7 +105,7 @@ exports.config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 20000
+    timeout: 60000
   },
   reporterOptions: {
     junit: {
