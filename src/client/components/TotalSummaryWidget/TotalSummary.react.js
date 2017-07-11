eea0dc1ee274ff4a5201efbc232b1ab53130b6cf
Mateusz <mateusz@whitebird.pl>
2017-07-11 10:00:57 +0200

    translations and group statuses

--- src/client/components/TotalSummaryWidget/TotalSummary.react.js
+++ src/client/components/TotalSummaryWidget/TotalSummary.react.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import _ from 'lodash';
 import { Col, Panel } from 'react-bootstrap';
+import { injectIntl } from 'react-intl';
 
 const sum = (campaigns, label) => campaigns.reduce( (result, value) => result + (value[label] || 0), 0);
 const formatValue = (campaigns, label) => sum(campaigns, label) || '-/-';
-const STATUSES = ['new', 'bounced', 'sent', 'read', 'loaded', 'onboarded'];
+const STATUSES = ['started', 'bounced', 'sent', 'read', 'loaded', 'onboarded'];
 
 const TotalSummary = ({campaigns}) => (
         <div className="panel panel-success">
