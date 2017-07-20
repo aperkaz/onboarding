4bebe80fea0acdb2ad56db72fb3f341655439d8a
Mateusz <mateusz@whitebird.pl>
2017-07-20 11:46:21 +0200

    RecentCampaigns - final touches

--- src/sharedComponents/recentCampaigns/component.js
+++ src/sharedComponents/recentCampaigns/component.js
@@ -18,7 +18,7 @@ class RecentCampaigns  extends React.Component {
     const component = this;
     this.context.i18n.register('RecentCampaigns', Messages);
     request.get('/onboarding/api/stats/campaigns').set('Accept', 'application/json').end(function(err, res) {
-      const data = formatDataRaw(res);
+      const data = formatDataRaw(res.body);
       component.setState({data: data});
     });
   }
@@ -26,22 +26,22 @@ class RecentCampaigns  extends React.Component {
   render() {
     return (
       <div className="panel panel-success">
-        <div className="panel-heading">{this.context.i18n.getMessage({ id: 'campaignDashboard.component.recentCampaigns'})}</div>
+        <div className="panel-heading">{this.context.i18n.getMessage('RecentCampaigns.title')}</div>
         <div className="panel-body">
-          <BarChart width={500} height={300} data={campaigns} margin={{ top: 5, right: 0, left: 0, bottom: 5 }}>
+          <BarChart width={500} height={300} data={this.state.data} margin={{ top: 5, right: 0, left: 0, bottom: 5 }}>
             <XAxis dataKey="name"/>
             <YAxis/>
             <CartesianGrid strokeDasharray="3 3"/>
             <Tooltip/>
             <Legend />
-            <Bar stackId="a" dataKey='started' barSize={20} fill='#8884d8' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.started' })} />
-            <Bar stackId="a" dataKey='bounced' barSize={20} fill='#459FD2' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.bounced' })} />
-            <Bar stackId="a" dataKey='read' barSize={20} fill='#F7783A' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.read' })} />
-            <Bar stackId="a" dataKey='loaded' barSize={20} fill='#A5A5A5' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.loaded' })} />
-            <Bar stackId="a" dataKey='registered' barSize={20} fill='#008000' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.registered' })} />
-            <Bar stackId="a" dataKey='serviceConfig' barSize={20} fill='#800080' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.serviceConfig' })} />
-            <Bar stackId="a" dataKey='onboarded' barSize={20} fill='#FFBB30' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.onboarded' })} />
-            <Bar stackId="a" dataKey='connected' barSize={20} fill='#FF1493' name={this.context.i18n.getMessage({ id: 'campaignDashboard.statuses.connected' })} />
+            <Bar stackId="a" dataKey='started' barSize={20} fill='#8884d8' name={this.context.i18n.getMessage('RecentCampaigns.started')} />
+            <Bar stackId="a" dataKey='bounced' barSize={20} fill='#459FD2' name={this.context.i18n.getMessage('RecentCampaigns.bounced')} />
+            <Bar stackId="a" dataKey='read' barSize={20} fill='#F7783A' name={this.context.i18n.getMessage('RecentCampaigns.read')} />
+            <Bar stackId="a" dataKey='loaded' barSize={20} fill='#A5A5A5' name={this.context.i18n.getMessage('RecentCampaigns.loaded')} />
+            <Bar stackId="a" dataKey='registered' barSize={20} fill='#008000' name={this.context.i18n.getMessage('RecentCampaigns.registered')} />
+            <Bar stackId="a" dataKey='serviceConfig' barSize={20} fill='#800080' name={this.context.i18n.getMessage('RecentCampaigns.serviceConfig')} />
+            <Bar stackId="a" dataKey='onboarded' barSize={20} fill='#FFBB30' name={this.context.i18n.getMessage('RecentCampaigns.onboarded')} />
+            <Bar stackId="a" dataKey='connected' barSize={20} fill='#FF1493' name={this.context.i18n.getMessage('RecentCampaigns.connected')} />
           </BarChart>
         </div>
       </div>
