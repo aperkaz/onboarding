e9c2433761e805a8b049ccbb6545fd95a060f558
Mateusz <mateusz@whitebird.pl>
2017-09-05 15:35:34 +0200

    after cleaning and first adjustment

--- src/client/components/CampaignContacts/CampaignContactList.react.js
+++ src/client/components/CampaignContacts/CampaignContactList.react.js
@@ -1,11 +1,12 @@
 import React, { PropTypes, Component } from 'react';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { ListGroup } from 'react-bootstrap';
 import './campaignContactEditor.css'
 import DeleteModal from '../common/DeleteModal.react';
 import _ from 'lodash';
 import { Pagination } from 'react-bootstrap';
 import { COUNT } from './../../constants/pagination';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
 
 export default class CampaignContactList extends Component {
   static propTypes = {
@@ -21,10 +22,6 @@ export default class CampaignContactList extends Component {
     super(props);
     this.state = {
       deleteModalOpen: false,
-      activePage: 1,
-      index: 0,
-      allContacts: [],
-      slicedContacts: []
     }
   }
   handleDeleteModalOpen(id) {
@@ -40,33 +37,7 @@ export default class CampaignContactList extends Component {
     this.handleDeleteModalClose();
   }
 
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.campaignContacts) {
-      let contacts = nextProps.campaignContacts.slice(0, COUNT);
-      if(!_.isEqual(this.props.campaignContacts, nextProps.campaignContacts)) {
-        this.setState({
-          allContacts: nextProps.campaignContacts,
-          slicedContacts: contacts
-        });
-      }
-    }
-  }
-
-// Handles onSelect function for pagination.
-  handleSelect(e) {
-    let i = (e - 1) * COUNT; //0,5
-    let contactArray;
-    let end = COUNT + i - 1; //0,10(6)
-    //let start = i==1?0:i
-    if(end > this.state.allContacts.length - 1) {
-      end = this.state.allContacts.length - 1;
-    }
-
-    contactArray = this.state.allContacts.slice(i, end + 1);
-    this.setState({ activePage: e, index: i, slicedContacts: contactArray });
-  }
-
-  renderActionPanel(cell, row) {
+  renderActionPanel(row) {
     const { onContactSelect, intl } = this.props;
     return (
       <div className="btn-group">
@@ -85,55 +56,47 @@ export default class CampaignContactList extends Component {
   }
 
   render() {
-    const { campaignContacts, selectedContact, onContactSelect, intl } = this.props;
+    const { campaignContacts, intl } = this.props;
 
-    if (_.size(this.state.slicedContacts) < 1) {
+    if (_.size(campaignContacts) < 1) {
       return null;
     }
 
+    const columns = [{
+      Header: intl.formatMessage({id:'campaignContactEditor.contactForm.email.label'}),
+      accessor: 'email' // String-based value accessors!
+    }, {
+      Header: intl.formatMessage({id:'campaignContactEditor.contactForm.companyName.label'}),
+      accessor: 'companyName'
+    }, {
+      Header: intl.formatMessage({id:'campaignContactEditor.contactForm.supplierId.label'}),
+      accessor: 'supplierId'
+    }, {
+      Header: intl.formatMessage({id:'campaignContactEditor.contactForm.status.label'}),
+      accessor: 'status'
+    }, {
+      sortable: false,
+      filterable: false,
+      Cell: row => (
+        this.renderActionPanel(row.original)
+      )
+    }]
+
     return (
       <div>
-        <div style={{float:'right',marginTop:'-54px'}}>
-          <Pagination
-          prev = {true}
-          next = {true}
-          bsClass='pagination'
-          maxButtons = {5}
-          items = {Math.ceil(campaignContacts.length/COUNT)}
-          activePage = {this.state.activePage}
-          onSelect = {(e)=>this.handleSelect(e)}
-          />
-        </div>
-        <BootstrapTable data={this.state.slicedContacts} bordered={false} condenesed striped>
-          <TableHeaderColumn width='150' dataField="email" isKey={true} dataSort={false}>
-            {intl.formatMessage({id:'campaignContactEditor.contactForm.email.label'})}
-          </TableHeaderColumn>
-
-          <TableHeaderColumn width='150' dataField="companyName" dataSort={false}>
-            {intl.formatMessage({id:'campaignContactEditor.contactForm.companyName.label'})}
-          </TableHeaderColumn>
-
-          <TableHeaderColumn width='150' dataField="supplierId">
-            {intl.formatMessage({id:'campaignContactEditor.contactForm.supplierId.label'})}
-          </TableHeaderColumn>
-
-          <TableHeaderColumn width='150' dataField="status" dataSort={false}>
-            {intl.formatMessage({id:'campaignContactEditor.contactForm.status.label'})}
-          </TableHeaderColumn>
-          <TableHeaderColumn width='150' dataAlign="right" dataFormat={::this.renderActionPanel}/>
-        </BootstrapTable>
-        <div style={{float:'right',marginTop:'-20px'}}>
-          <Pagination
-            prev = {true}
-            next = {true}
-            bsClass='pagination'
-            maxButtons = {5}
-            items = {Math.ceil(campaignContacts.length/COUNT)}
-            activePage = {this.state.activePage}
-            onSelect = {(e)=>this.handleSelect(e)}
-          />
-        </div>
-        <div className="clearfix"></div>
+        <ReactTable
+          data={campaignContacts}
+          columns={columns}
+          minRows={1}
+          defaultPageSize={10}
+          showPagination={true}
+          showPaginationTop
+          showPaginationBottom
+          filterable
+          showPageSizeOptions={false}
+          showPageJump={false}
+          className="-striped -highlight ocbn table"
+        />
         <DeleteModal
           isOpen={this.state.deleteModalOpen}
           onDelete={() => {this.onDeleteContact(this.state.id)}}
