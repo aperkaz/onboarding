ce5e4b3747e6c6f7a43875249b61ce088eed3e95
nczeranka <norbert.czeranka@opuscapita.com>
2017-10-02 15:35:34 +0000

    fixed user query, fixed REST result handling, added status in export

--- src/server/routes/campaignContact.js
+++ src/server/routes/campaignContact.js
@@ -97,7 +97,7 @@ ContactsWebApi.prototype.exportContacts = function(req, res)
 
     if(customerId)
     {
-      getExport(req.params.campaignId, req.opuscapita.serviceClient, this.db).
+      getExport(customerId, req.params.campaignId, req.opuscapita.serviceClient, this.db).
       then(csvData => {
         res.set('Content-disposition', 'attachment; filename="data.csv"');
         res.set('Content-type', 'text/csv;charset=utf-8');
